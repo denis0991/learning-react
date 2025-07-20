f809e8f2107fd2990c061fbfb7c947934359f118
Denis Nasonov <brain0991@gmail.com>
2025-07-20 23:55:59 +0300

    refactor: adjust app and search test

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, test, beforeEach } from 'vitest';
+import '@testing-library/jest-dom/vitest';
 import { App } from '../App';
 import userEvent from '@testing-library/user-event';
 
@@ -23,5 +24,20 @@ describe('App with localStorage', () => {
     await user.type(input, 'Owon');
 
     expect(input.value).toBe('Owon');
+  });
+  test('should save input value to localStorage on search button click', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const input = screen.getByPlaceholderText('search..');
+    const button = screen.getByRole('button', { name: /Search/i });
+
+    await user.clear(input);
+    await user.type(input, 'Abalone');
+    await user.click(button);
+
+    const saved = localStorage.getItem('request');
+    expect(saved).toBe(JSON.stringify('Abalone'));
   });
 });
