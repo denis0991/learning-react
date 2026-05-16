25f335dcc09264bdce8205652732ace15b2988f1
Denis Nasonov <brain0991@gmail.com>
2026-05-16 12:18:09 +0300

    feat: convert the header into a functional component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,13 +1,11 @@
-import React, { type ReactNode } from 'react';
+import { type ReactElement } from 'react';
 import './header.styles.css';
 
-export class Header extends React.Component {
-  render(): ReactNode {
-    return (
-      <header>
-        <h1>Star Trek</h1>
-        <h2>Animals</h2>
-      </header>
-    );
-  }
+export function Header(): ReactElement {
+  return (
+    <header>
+      <h1>Star Trek</h1>
+      <h2>Animals</h2>
+    </header>
+  );
 }
