4fd3a01ecb531c2318c0a03307a0530d6bf3b50d
Denis Nasonov <brain0991@gmail.com>
2026-05-22 22:56:46 +0300

    feat: add test for not-found page

---
+++ src/components/not-found/not-found.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { NotFound } from './not-found.component';
+import { MemoryRouter } from 'react-router-dom';
+
+it('should render the title and content', () => {
+  render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText('404')).toBeInTheDocument();
+  expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+    'Page Not Found'
+  );
+  expect(screen.getByRole('link')).toHaveAttribute('href', '/?page=1');
+});
