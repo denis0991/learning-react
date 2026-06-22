8830bbdb56a744423e65cf24ac2a57bfdb0baed8
Denis Nasonov <brain0991@gmail.com>
2026-06-22 19:23:09 +0300

    feat: add next/image logo to Header

--- nextjs-ssr/src/components/header/header.component.tsx
+++ nextjs-ssr/src/components/header/header.component.tsx
@@ -2,6 +2,7 @@ import { type ReactElement } from 'react';
 import './header.styles.css';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
+import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import { ThemeSwitcher } from '../themeSwitcher/themeSwitcher.component';
 import { useTheme } from '@/context/theme-context';
@@ -30,6 +31,16 @@ export function Header({
 
   return (
     <header className={`app-header ${theme}`}>
+      <div className="header__logo">
+        <Image
+          src="/images/star-trek-symbol.svg"
+          alt="Star Trek Logo"
+          width={40}
+          height={40}
+          priority
+          className="header__logo-image"
+        />
+      </div>
       <h1>Star Trek</h1>
       <h2>Animals</h2>
       <nav className="header__nav-menu">
